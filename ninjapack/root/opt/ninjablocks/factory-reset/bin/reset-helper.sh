f2a52fa9ddd6c1a0790761d0cd255289876c212a
Jon Seymour <jon.seymour@gmail.com>
2014-12-03 12:57:48 +1100

    fix: add setup of reset button.
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>

--- ninjapack/root/opt/ninjablocks/factory-reset/bin/reset-helper.sh
+++ ninjapack/root/opt/ninjablocks/factory-reset/bin/reset-helper.sh
@@ -282,9 +282,9 @@ main()
 		if test -e /sys/kernel/debug/omap_mux/xdma_event_intr1; then
 			# in later kernels (3.12) this path won't exist
 			echo 37 > /sys/kernel/debug/omap_mux/xdma_event_intr1
-			echo 20 > /sys/class/gpio/export
-			echo in > /sys/class/gpio/gpio20/direction
 		fi
+		echo 20 > /sys/class/gpio/export
+		echo in > /sys/class/gpio/gpio20/direction
 
 		# to read the reset button
 		# cat /sys/class/gpio/gpio20/value
