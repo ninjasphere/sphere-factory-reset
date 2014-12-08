9ef0769b7033afb7b504f38c640e34a3a075d44b
Jon Seymour <jon.seymour@gmail.com>
2014-12-09 00:16:49 +1100

    Create a script for repacking a .deb file as a .ipkg file.
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>

---
+++ ninjapack/root/opt/ninjablocks/factory-reset/bin/ipkg.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+die() {
+	echo "$*" 1>&2
+	exit 1
+}
+
+repack() {
+	deb=$1
+
+	test -f "$deb" || die "usage: repack {.deb} file"
+
+	package=$(basename $deb .deb)
+	work=/tmp/ipkg.work.$$
+
+	trap "rm -rf $work" EXIT
+
+	cp $deb $work &&
+	pushd $work &&
+	ar x $(basename $deb) &&
+	tar -cvzf ${package}.ipk ./debian-binary ./data.tar.gz ./control.tar.gz &&
+	popd &&
+	cp $work/${package}.ipk . &&
+	echo $(pwd)/${package}.ipk
+}
+
+main() {
+	case "$1" in
+	repack)
+		shift 1
+		repack "$@"
+	;;
+	*)
+		die "usage: repack .deb"
+	;;
+	easc
+}
+
+main "$@"
