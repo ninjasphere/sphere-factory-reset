baae6f8301a84c7ad2af4a44fc4ee927ba07a027
Jon Seymour <jon.seymour@gmail.com>
2014-12-17 17:02:32 +1100

    Extract the timestamp of the other script without calling it.
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>

--- ninjapack/root/opt/ninjablocks/factory-reset/bin/recovery.sh
+++ ninjapack/root/opt/ninjablocks/factory-reset/bin/recovery.sh
@@ -888,14 +888,22 @@ choose_script() {
 
 	script=$1
 	if ${RECOVERY_ENABLE_SCRIPT_PHASES}; then
-		self=$(recovery_sh_timestamp) # our own timestamp
-		other=$(sh $script recovery-sh-timestamp) # the potential delegate's timestamp
-		resolution=$(resolve_delegation "$self" "$other" "${RECOVERY_ARCHIVE_DELEGATION_RULE}") # the resolved timestamp
+		resolution=$0 && #failsafe
+		self=$(recovery_sh_timestamp) && # our own timestamp
+		other=$(
+			other_home=$(cd $(dirname "$script")/..; pwd) &&
+			test -f "${other_home}/etc/timestamp" &&
+			other_timestamp=$(cat "${other_home}/etc/timestamp") &&
+			echo "$script"
+		) &&		 # the potential delegate's times
+		resolution=$(resolve_delegation "$self" "$other" "${RECOVERY_ARCHIVE_DELEGATION_RULE}") && # the resolved timestamp
 		if test "$resolution" = "$self"; then
 			progress "3603" "Found other script ('$other') but continuing with ('$self') because of rule ('${RECOVERY_ARCHIVE_DELEGATION_RULE}')"
+			resolution="$0"
 		else
+			resolution="$script"
 			progress "3609" "Delegating to alternative script '$script'."
-		fi
+		fi &&
 		echo "$resolution"
 	else
 		progress "3602" "Script phases are disabled by RECOVERY_ENABLE_SCRIPT_PHASES. Using '$0' instead of '$script'."
